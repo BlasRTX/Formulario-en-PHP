94d94309468fd1ffa5cc8ed0a74b88703aacb67e
Blas Arias Zambra <blasx256@gmail.com>
2023-12-27 14:36:59 -0300

    Implementacion formulario con CSS

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola</title>
-    <link rel="stylesheet" hrefr="style.css">
+    <title>Formulario</title>
+    <link rel="stylesheet" href="style.css">
 
 </head>
 
@@ -16,7 +16,7 @@
         <p>Iniciar registro</p>
 
         <div class="input-wrapped">
-            <input type="text" name="name" placeholder="Nombre" alt=""
+            <input type="text" name="name" placeholder="Nombre"
         </div>
 
         <div class="input-wrapped">
@@ -39,7 +39,7 @@
             <input type="text" name="password" placeholder="Contrasenia"
         </div>
 
-        <input class="btnRegister" type="submit" namespace="register" value="Enviar">
+        <input class="btn" type="submit" namespace="register" value="Enviar">
 
     </form>
 
