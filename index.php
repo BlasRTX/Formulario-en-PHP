7dcf31fd7d1526cb24544f74da7ba10d7487b112
Blas Arias Zambra <blasx256@gmail.com>
2023-12-27 13:42:29 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola</title>
+    <link rel="stylesheet" hrefr="style.css">
+
+</head>
+
+<body>
+
+    <form method="post">
+        <h2>Formulario</h2>
+        <p>Iniciar registro</p>
+
+        <div class="input-wrapped">
+            <input type="text" name="name" placeholder="Nombre" alt=""
+        </div>
+
+        <div class="input-wrapped">
+            <input type="text" name="add" placeholder="Apellido"
+        </div>
+
+        <div class="input-wrapped">
+            <input type="text" name="address" placeholder="Direccion"
+        </div>
+
+        <div class="input-wrapped">
+            <input type="text" name="phone" placeholder="Telefono"
+        </div>
+
+        <div class="input-wrapped">
+            <input type="text" name="email" placeholder="Correo electronico"
+        </div>
+
+        <div class="input-wrapped">
+            <input type="text" name="password" placeholder="Contrasenia"
+        </div>
+
+        <input class="btnRegister" type="submit" namespace="register" value="Enviar">
+
+    </form>
+
+
+
+
+</body>
+
+</html>
