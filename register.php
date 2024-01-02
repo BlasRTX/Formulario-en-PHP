61f057308f7eb848305f4ada624bcadb5e19d4ee
Blas Arias Zambra <blasx256@gmail.com>
2024-01-02 20:48:44 -0300

    Modificacion pendiente register

--- register.php
+++ register.php
@@ -20,27 +20,30 @@ if (isset($_POST['regist'])) {
         $email = trim($_POST['email']);
         $password = trim($_POST['password']);
         $date = date("d/m/y");
-        /*Insertar variables en la base de datos*/
-        $queryinsert = "INSERT INTO usuario(nombre, direccion, telefono, correo, clave, fecha)
-                  VALUES('$name', '$address', '$phone', '$email', '$password', '$date')";
-        /*Consultar insercion de los datos*/
-        $result = mysqli_query($conex, $queryinsert);
-        /*Si realizar*/
-        if($result){
-            /*Insercion correcta.*/    
-            ?>  
-            <h3 class="success" >Registro realizado con exito.</h3>
+
+        /*Consultar en la base de datos*/
+        $query = "SELECT nombre, direccion, telefono, correo, clave FROM `usuario` WHERE correo = 'email' OR clave = 'password' ";
+        /*Consultar*/
+        $resultquery = mysqli_query($conex, $query);
+        /*SI el dato existe en la base de datos*/
+        if(($resultquery) && ($queryrow = mysqli_num_rows($resultquery)) > 0){
+        /*Si el dato existe en la base de datos*/
+        ?>  
+            <h3 class="success" >El usuario ya existe.</h3>
             <?php
-            }else{
-                /*Insercion erronea.*/
-                ?>
-                <h3 class="error" >Que pasa master? Ocurrio un error. Volver a intentar.</h3>
-                <?php
-            }
         }
-        else{
-            /*Insercion incompleta por falta de datos en formulario.*/  
-            ?>
+    }else{
+        /*Insertar datos en el formulario.*/  
+        $queryinsert = "INSERT INTO usuario(nombre, direccion, telefono, correo, clave, fecha)
+        VALUES('$name', '$address', '$phone', '$email', '$password', '$date')";
+
+        $resultqueryins = mysqli_query($conex, $queryinsert);
+
+        if($resultqueryins > 0){
+           /*Insercion correcta de datos en el formulario.*/  
+        }
+           /*Error de los datos en el formulario.*/  
+             ?>
             <h3 class="error" >Rellene el formulario correctamente.</h3>
             <?php
         }
