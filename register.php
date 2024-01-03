7124ef717bf7509bb67b5ce24c3bdede321859ec
Blas Arias Zambra <blasx256@gmail.com>
2024-01-03 12:09:19 -0300

    Modificacion de validaciones en errores de sintaxys y en sql. Funcional, revisar sql

--- register.php
+++ register.php
@@ -15,23 +15,57 @@ if (isset($_POST['regist'])) {
         if($name != "" ||  $address != "" || $phone != "" || $email != "" || $password != ""){   
              /*Validar si el nombre fue inscrito correctamente*/  
             if(preg_match('/^[a-zA-Z\s]+$/', $name)){
+
                  /*Validar si la direccion fue inscrita correctamente**/
                 if(preg_match('/^[a-zA-Z0-9\s\.,#\-]+$/', $address)){
+
                     /*Validar si el telefono fue inscrito correctamente**/
                     if(preg_match('/^(\+?56)?(\s?)(0?9)(\s?)[1-9]\d{3}(\s?)\d{4}$/', $phone) || preg_match('/^\+?\d{1,3}[-\s]?\d{3,14}$/', $phone) ){
+
                         /*Validar si el correo fue inscrito correctamente**/
-                        if( (strpos($email, "@") == false) || filtrer_var(FILTER_VALIDATE_EMAIL, $email)){
+                        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+
                             /*Validar si la clave fue inscrita correctamente**/
                             /*Valor minimo de caracteres: 8**/
                             /*Mayuscula: 1**/
                             /*Minuscula: 4**/
                             /*Numeros disponibles: 3**/
                             /*Ejemplo: Holaa123**/
-                            if((strlen($password) >= 8 ) && preg_match('/[A-Za-z]+/', $password) && preg_match('/[0-9]+/', $password)){
-                                ?>
-                                <h3 class="success" >YESSS.</h3>
-                                <?php
+                            if((strlen($password) >= 8 ) && (preg_match('/[A-Za-z]+/', $password) && preg_match('/[0-9]+/', $password))){
 
+                                /*Consulta en la base de datos*/
+                                $query = "SELECT * FROM usuario WHERE correo = 'email' || clave = 'password' ";
+
+                                $querycons = mysqli_query($conex, $query);
+
+                                $resultquery = mysqli_fetch_assoc($querycons);
+
+                                 /*SI el dato existe en la base de datos*/
+                                if($resultquery > 0){
+                                    ?>
+                                    <h3 class="error" >No mi rey, ya existe.</h3>
+                                    <?php
+                                }else{
+                                    /*Insertar nuevo usuario en el formulario*/
+                                    $queryinsert = "INSERT INTO usuario(nombre, direccion, telefono, correo, clave, fecha)
+                                                    VALUES('$name', '$address', '$phone', '$email', '$password', '$date')";
+
+                                                    $resultqueryins = mysqli_query($conex, $queryinsert);
+
+                                                    if($resultqueryins > 0){
+                                                             /*Insercion correcta de datos en el formulario.*/
+                                                            ?>
+                                                           <h3 class="success" >Usuario registrado.</h3>
+                                                           <?php
+                                                    }else{
+                                                        /*Error de los datos en el formulario.*/  
+                                                            ?>
+                                                           <h3 class="error" >Error.</h3>
+                                                           <?php
+                                                    }
+                                                    
+                                }
+                        
                             }else{
                                 ?>
                                 <h3 class="error" >clave no fue rellenado correctamente.</h3>
