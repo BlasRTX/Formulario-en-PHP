f59bb809836235b96678f55d9bd65b522652afbe
Blas Arias Zambra <blasx256@gmail.com>
2024-01-25 10:10:40 -0300

    Modal y validaciones en web

--- template/form_create_user.php
+++ template/form_create_user.php
@@ -1,3 +1,9 @@
+<?php
+include("../database/register_user_web.php");
+?>
+<!-- Modal Agregar Nuevo Usuario -->
+<div>
+<form autocomplete="off" method="POST">
 <!-- Modal Agregar Nuevo Usuario -->
 <div class="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -8,27 +14,32 @@
       </div>
       <div class="modal-body">
       <!-- Modal Body -->
-      <label for="">Nombre</label>
-      <input type="text" class="form-control" name="nombre_Update" value="" placeholder="Ejemplo: Jennifer Lawrance"></input>
+      <!-- Nombre -->
+        <label for="validationCustom01" class="form-label">Nombre</label>
+        <input type="text" class="form-control" name="nameCreate" placeholder="Ejemplo: Jennifer Lawrance"></input>
+      <!-- Nombre -->
 
       <label for="">Direccion</label>
-      <input type="text" class="form-control" name="direccion_Update" value="" placeholder="Ejemplo: La Coila #681"></input>
+      <input type="text" class="form-control" name="addressCreate"  placeholder="Ejemplo: La Coila #681"></input>
 
       <label for="">Telefono</label>
-      <input type="text" class="form-control" name="telefono_Update" value="" placeholder="Ejemplo: +56923456789" ></input>
+      <input type="text" class="form-control" name="phoneCreate"  placeholder="Ejemplo: +56923456789" ></input>
 
       <label for="">Correo</label>
-      <input type="text" class="form-control" name="correo_Update" value="" placeholder="Ejemplo: nicolas.barrios@alumnos.ucn.cl" ></input>
+      <input type="text" class="form-control" name="emailCreate"  placeholder="Ejemplo: nicolas.barrios@alumnos.ucn.cl" ></input>
 
       <label for="">Clave</label>
-      <input type="text" class="form-control" name="clave_Update" value="" placeholder="Ejemplo: Hola123"></input>
+      <input type="text" class="form-control" name="passwordCreate"  placeholder="Ejemplo: Hola123"></input>
     <!-- Modal Body -->
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-        <button type="button" class="btn btn-primary">Agregar</button>
+        <input class="btn btn-primary" type="submit" name="btnCreate" value="Agregar">
       </div>
     </div>
   </div>
 </div>
 <!-- Modal Agregar Nuevo Usuario -->
+</form>
+
+</div>
